81ad26e57806f65267260cdd38086f4f10885fde
Jason <jasonyl13579@gmail.com>
2021-10-23 17:18:00 -0400

    add upload picture

--- src/index.py
+++ src/index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sys
 app = Flask(__name__)
 
 from .receipt_recognition import get_items
@@ -7,9 +8,16 @@ from .receipt_recognition import get_items
 def index():
   return render_template('index.html')
 
-@app.route('/user_id/image', methods = ['POST'])
-def process_image():
-  pass
+
+@app.route('/user_id/upload_images', methods = ['POST'])
+def process_images():
+  files = request.files.getlist("file[]")
+  for file in files:
+    #print (file,sys.stderr)
+    fs = file.read()
+    res = get_items(fs)
+    #print (res,sys.stderr)
+  return "OK"
 
 @app.route('/user_id/search/<search_str>', methods = ['GET'])
 def get_list(search_str):
