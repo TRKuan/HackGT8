d7aa850aee88697955ae208cf0c6593ba2bfcb7e
Jason <jasonyl13579@gmail.com>
2021-10-23 15:35:53 -0400

    add flask endpoint and simple html

--- src/index.py
+++ src/index.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def index():
   return render_template('index.html',)
 
+@app.route('/user_id/image', methods = ['POST'])
+def process_image():
+  pass
+
+@app.route('/user_id/search/<search_str>', methods = ['GET'])
+def get_list(search_str):
+  pass
+
+#  return 
 if __name__ == '__main__':
     app.run()
